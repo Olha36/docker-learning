02d4c351e6ed1e6436d27ff136b9e43c8a4a02a4
Olha36 <olhakucheruk36@gmail.com>
2023-01-18 16:23:56 +0000

    readme instruction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React...222
+          Learn React32
         </a>
       </header>
     </div>
